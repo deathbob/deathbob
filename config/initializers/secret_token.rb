781298cdf2b3378585da6eadb06678202b3b3ed1
Bob Larrick <larrick@gmail.com>
2013-01-18 14:01:45 -0500

    switching to clojure

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Deathbob::Application.config.secret_token = '05d1e0ba4b116fe3d766afe11b4e7852a779b4836285f2aaca4eb90615b25282f6441cc97f0c23aa07f2f0f312be744d94b560d8927165d1b6b42ca43e20a415'
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject heroku "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+  [ring/ring-jetty-adapter "1.1.0"]
+  ])
