18397b63c71a521e78461fa95d80f0eac1341d49
Bob Larrick <larrick@gmail.com>
2013-04-12 15:08:32 -0400

    asdf

--- src/deathbob/handler.clj
+++ src/deathbob/handler.clj
@@ -9,6 +9,11 @@
 
 (def coords (atom {}));; hash-map
 
+def index(ctxt)
+  [:div#message].each do |x|
+    x.innner_html = ctxt[:message]
+  end
+end
 (html/deftemplate index "deathbob/template1.html"
   [ctxt]
   [:div#message] (html/content (:message ctxt)))
